d99b468ca75e7c65a4c0a99127838470faaf8192
J. Alex Carney <38840263+JAlexCarney@users.noreply.github.com>
2019-10-21 15:44:57 -0700

    Added attack
    player can now attack using a click.

---
+++ Druid_Theives_170/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.transform.gameObject, 0.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Hazard")
+        {
+            RectTransform trans = GameObject.FindGameObjectWithTag("BossHealth").GetComponent<RectTransform>();
+            trans.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, trans.rect.width - 10);
+        }
+    }
+
+}
---
+++ Druid_Theives_170/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject attackPrefab;
+    private int delayCount = 0;
+    private int delayAmount = 60;
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) && delayCount >= delayAmount)
+        {
+            delayCount = 0;
+            Debug.Log("Click");
+            Vector3 input = Input.mousePosition;
+            Vector3 pos = transform.position;
+            input.z = 0;
+            input = Camera.main.ScreenToWorldPoint(input);
+            Vector2 dir = new Vector2(input.x - pos.x, input.y - pos.y).normalized;
+            GameObject attack = Instantiate(attackPrefab);
+            attack.transform.position = new Vector3(pos.x + 2*dir.x, pos.y + 2*dir.y);
+            attack.transform.eulerAngles = new Vector3(0, 0, Vector2.Angle(new Vector2(0,1), dir) + 180f);
+            attack.transform.localScale = new Vector3(1, 1, 1);
+        }
+        if(delayCount < delayAmount)
+        {
+            delayCount++;
+        }
+        
+    }
+}
